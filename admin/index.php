db060cad780b53ed536a9cb7339cb9db66932936
lsces <lester@lsces.co.uk>
2016-02-07 22:07:02 +0000

    Move from Smarty2 to Smarty3 function style

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@ require_once( '../../kernel/setup_inc.php' );
 
 $gBitSystem->verifyPermission( 'p_admin' );
 
-$gBitSmarty->assign_by_ref( 'gLibertySystem', $gLibertySystem );
+$gBitSmarty->assignByRef( 'gLibertySystem', $gLibertySystem );
 
 $gBitSystem->display( 'bitpackage:quicktags/select_format.tpl', NULL, array( 'display_mode' => 'admin' ));
 
